e0e79e86c575313e30a3db9ec0060720f4aef583
Bryle <b2castro@ucsd.edu>
2018-07-09 22:48:19 -0700

    Almost done

--- create_database.js
+++ create_database.js
@@ -1,30 +1,31 @@
-// Node.js + Express server backend for passwdAsService
-// v1 - use SQLite (https://www.sqlite.org/index.html) as a database
-//
-// Bryle Castro
-
-// run this once to create the initial database as the users.db file
-//   node create_database.js
-
-// to clear the database, simply delete the users.db file:
-//   rm users.db
-
+/* File Name: create_database.js
+ * Node.js + Express server backend for passwdAsService
+ * v1 - use SQLite (https://www.sqlite.org/index.html) as a database
+ *
+ *
+ * run this once to create the initial database as the users.db file
+ *  - node create_database.js
+ *
+ * to clear the database, simply delete the users.db file:
+ *  - rm users.db
+ *   
+ * Author: Bryle Castro
+ */
 const sqlite3 = require('sqlite3');
-const db = new sqlite3.Database('users.db');
-const dbGroups = new sqlite3.Database('groups.db');
+const db = new sqlite3.Database('users.db'); //stores data from etc/passwd.txt
+const dbGroups = new sqlite3.Database('groups.db'); //stores data from etc/groups.txt
+
 // run each database statement *serially* one after another
-// (if you don't do this, then all statements will run in parallel,
-//  which we don't want)
 db.serialize(() => {
-  // create a new database table:
-  db.run("CREATE TABLE users (name TEXT UNIQUE, uid TEXT UNIQUE, gid TEXT, comment TEXT, home TEXT UNIQUE, shell TEXT)");
+    // create a new database table:
+    db.run("CREATE TABLE users (name TEXT UNIQUE, uid TEXT UNIQUE, gid TEXT, comment TEXT, home TEXT UNIQUE, shell TEXT)");
 
 });
 
 dbGroups.serialize(() => {
-  // create a new database table:
-  dbGroups.run("CREATE TABLE groups (name TEXT UNIQUE, gid TEXT, member TEXT)");
-    
+    // create a new database table:
+    dbGroups.run("CREATE TABLE groups (name TEXT UNIQUE, gid TEXT, member TEXT)");
+
 });
 
 dbGroups.close();
--- server.js
+++ server.js
@@ -1,4 +1,12 @@
-
+/**
+ * Filename: server.js
+ *
+ * Description: This file handles reading the etc/passwd and etc/groups files and placing their respective
+ *              data into storage for later use. Moreover, this file uses Node.js + Express as server backend 
+ *              for the application.
+ *
+ * Author: Bryle Castro
+ **/
 const express = require('express');
 const app = express();
 
@@ -11,125 +19,256 @@ const dbGroups = new sqlite3.Database('groups.db');
 
 
 app.use(express.static('static_files'));
+
+/*Insert or update data stored in users database based on changes made to password.txt*/
 var lineReader = require('readline').createInterface({
-  input: require('fs').createReadStream('etc/passwd.txt')
+    input: require('fs').createReadStream('etc/passwd.txt')
+});
+lineReader.on('line', function(line) {
+    let str = line;
+    let array = str.split(':');
+
+    db.prepare("INSERT OR REPLACE INTO users (name,uid,gid,comment,home,shell) VALUES(?,?,?,?,?,?)").run(array[0], array[2], array[3], array[4], array[5], array[6]).finalize();
+
 });
 
-lineReader.on('line', function (line) {
-let str = line;
-let array = str.split(':');
-    
-db.prepare("INSERT OR REPLACE INTO users (name,uid,gid,comment,home,shell) VALUES(?,?,?,?,?,?)").run(array[0],array[2],array[3],array[4],array[5],array[6]).finalize();
-    
-  /* print them out to confirm their contents:
-  db.each("SELECT name, uid, gid, comment, home, shell FROM users", (err, row) => {
-      console.log(row.name + ": " + row.uid + ' - ' + row.gid + ' - ' + row.comment + ' - ' + row.home + ' - ' + row.shell );
-  });*/ 
-    
-}); 
-
-
+/*Insert or update data stored in users database based on changes made to groups.txt*/
 var lineReaderGroups = require('readline').createInterface({
-  input: require('fs').createReadStream('etc/groups.txt')
+    input: require('fs').createReadStream('etc/groups.txt')
+});
+lineReaderGroups.on('line', function(line) {
+    let str = line;
+    let array = str.split(':');
+
+    dbGroups.prepare("INSERT OR REPLACE INTO groups (name,gid,member) VALUES(?,?,?)").run(array[0], array[2], array[3]).finalize();
 });
 
-lineReaderGroups.on('line', function (line) {
-let str = line;
-let array = str.split(':');
-    
-dbGroups.prepare("INSERT OR REPLACE INTO groups (name,gid,member) VALUES(?,?,?)").run(array[0],array[2],array[3]).finalize();
-    
-  /* print them out to confirm their contents:
-  dbGroups.each("SELECT name, uid, gid, comment, home, shell FROM groups", (err, row) => {
-      console.log(row.name + ": " + ' - ' + row.gid + ' - ' + row.member);
-  });*/ 
-    
-}); 
 
+/* Get a list of all groups on the system, a defined by /etc/group.txt */
 app.get('/groups', (req, res) => {
-   
-    
-  // db.all() fetches all results from an SQL query into the 'rows' variable:
-  dbGroups.all('SELECT * FROM groups', (err, rows) => {
-    console.log(rows);
-    const allUsernames = rows.map(e => e);
-    console.log(allUsernames);
-    res.send(allUsernames);
-  });
+    // db.all() fetches all results from an SQL query into the 'rows' variable:
+    dbGroups.all('SELECT * FROM groups', (err, rows) => {
+        console.log(rows);
+        const allUsernames = rows.map(e => e);
+        console.log(allUsernames);
+        res.send(allUsernames);
+    });
+});
+
+/* Get all the groups.*/
+app.get('/uidGroup', (req, res) => {
+    // db.all() fetches all results from an SQL query into the 'rows' variable:
+    dbGroups.all('SELECT * FROM groups', (err, rows) => {
+        console.log("Object returned: " + rows);
+        const allUsernames = rows.map(e => e);
+        console.log(allUsernames);
+        res.send(allUsernames);
+    });
 });
 
 app.get('/users', (req, res) => {
-   
-    
-  // db.all() fetches all results from an SQL query into the 'rows' variable:
-  db.all('SELECT * FROM users', (err, rows) => {
-    console.log(rows);
-    const allUsernames = rows.map(e => e);
-    console.log(allUsernames);
-    res.send(allUsernames);
-  });
+
+    // db.all() fetches all results from an SQL query into the 'rows' variable:
+    db.all('SELECT * FROM users', (err, rows) => {
+        console.log(rows);
+        const allUsernames = rows.map(e => e);
+        console.log(allUsernames);
+        res.send(allUsernames);
+    });
 });
 
-/*
+/* Handles functionality of getting group of a user*/
+function getGroups(res, NAME) {
+
+    var jsonString = JSON.stringify(NAME);
+    jsonString = jsonString.trim();
+    jsonString = jsonString.split(':')[1]
+    jsonString = jsonString.split('"')[1]
+    jsonString = jsonString.split('"')[0]
+    let error404 = "User " + jsonString + " not found";
+    dbGroups.all(
+
+        'SELECT * FROM groups WHERE name=$name',
+        // parameters to SQL query:
+        {
+            $name: jsonString
+        },
+        // callback function to run when the query finishes:
+        (err, rows) => {
+            console.log(rows);
+            if (rows.length > 0) {
+                res.send(rows[0])
+            } else {
+                res.send({
+                    error404
+                }); // failed, so return an empty object instead of undefined
+            }
+        }
+    );
+}
+
+/* Get all the groups for a given uid.*/
+app.get('/uidGroup/:userid', (req, res) => {
+    const idToLookup = req.params.userid; // matches ':userid' above
+
+    let error404 = "Error 404: User with uid " + idToLookup + " not found";
+    var x;
+    // db.all() fetches all results from an SQL query into the 'rows' variable:
+    db.all(
+
+        'SELECT name FROM users WHERE uid=$uid',
+        // parameters to SQL query:
+        {
+            $uid: idToLookup
+        },
+        // callback function to run when the query finishes:
+        (err, rows) => {
+
+            if (rows.length > 0) {
+                getGroups(res, rows[0]);
+            } else {
+                res.send({
+                    error404
+                }); // failed, so return an empty object instead of undefined
+            }
+        }
+    );
+
+});
+
+/* Get a single user with <uid>. Return 404 if <uid> is not found. */
 app.get('/users/:userid', (req, res) => {
-  const idToLookup = req.params.userid; // matches ':userid' above
-   let error404 = "Error 404: User with uid " +idToLookup + " not found";
-  // db.all() fetches all results from an SQL query into the 'rows' variable:
-  db.all(
-    'SELECT * FROM users WHERE uid=$uid',
-    // parameters to SQL query:
-    {
-      $uid: idToLookup
-    },
-    // callback function to run when the query finishes:
-    (err, rows) => {
-      console.log(rows);
-      if (rows.length > 0) {
-        res.send(rows[0]);
-      } else {
-        res.send({error404}); // failed, so return an empty object instead of undefined
-      }
-    }
-  );
-});*/
-
-app.get('users/:name?/:userid?/:gid?/:comment?/:home?/:shell?', (req, res) => {
-  const idToLookup = req.params.userid; // matches ':userid' above
-  const nameToLookup = req.params.name;
-  const gidToLookup = req.params.gid;
-  const commentToLookup = req.params.comment;
-  const homeToLookup = req.params.home;
-  const shellToLookup = req.params.shell;
-    
-   let error404 = "Error 404: User not found";
-  // db.all() fetches all results from an SQL query into the 'rows' variable:
-  db.all(
-    'SELECT * FROM users WHERE name=$name AND uid=$uid',
-    // parameters to SQL query:
-    {
-      $uid: idToLookup,
-      $name: nameToLookup
-      /*$gid:  gidToLookup,
-      $comment: commentToLookup,
-      $home: homeToLookup,
-      $shell: shellToLookup*/
-                 
-    },
-    // callback function to run when the query finishes:
-    (err, rows) => {
-      console.log(rows);
-      if (rows.length > 0) {
-        res.send(rows[0]);
-      } else {
-        res.send({error404}); // failed, so return an empty object instead of undefined
-      }
-    }
-  );
+    const idToLookup = req.params.userid; // matches ':userid' above
+    let error404 = "Error 404: User with uid " + idToLookup + " not found";
+    console.log("this one " + idToLookup);
+    // db.all() fetches all results from an SQL query into the 'rows' variable:
+    db.all(
+        'SELECT * FROM users WHERE uid=$uid',
+        // parameters to SQL query:
+        {
+            $uid: idToLookup
+        },
+        // callback function to run when the query finishes:
+        (err, rows) => {
+            console.log(rows);
+            if (rows.length > 0) {
+                res.send(rows[0]);
+            } else {
+                res.send({
+                    error404
+                }); // failed, so return an empty object instead of undefined
+            }
+        }
+    );
 });
 
+/*Get users based on specifed optional query parameters*/
+app.get('/query', (req, res) => {
+    let nameToLookup = req.query.param1;
+    let idToLookup = req.query.param2;
+    let gidToLookup = req.query.param3;
+    let commentToLookup = req.query.param4;
+    let homeToLookup = req.query.param5;
+    let shellToLookup = req.query.param6;
+
+    //console.log(req.query.param5);    
+    if (nameToLookup == '') {
+        nameToLookup = null;
+    }
+    if (idToLookup == '') {
+        idToLookup = null;
+    }
+    if (gidToLookup == '') {
+        gidToLookup = null;
+    }
+    if (commentToLookup == '') {
+        commentToLookup = null;
+    }
+    if (homeToLookup == '') {
+        homeToLookup = null;
+    }
+    if (shellToLookup == '') {
+        shellToLookup = null;
+    }
+
+    let error404 = "Error 404: User not found";
+    // db.all() fetches all results from an SQL query into the 'rows' variable:
+    db.all(
+        'SELECT * FROM users WHERE (name = $name OR $name IS NULL) AND (uid = $uid OR $uid IS NULL) AND (gid = $gid OR $gid IS NULL) AND (comment = $comment OR $comment IS NULL) AND (home = $home OR $home IS NULL) AND (shell = $shell OR $shell IS NULL)',
+        // parameters to SQL query:
+        {
+            $uid: idToLookup,
+            $name: nameToLookup,
+            $gid: gidToLookup,
+            $comment: commentToLookup,
+            $home: homeToLookup,
+            $shell: shellToLookup
+
+        },
+        // callback function to run when the query finishes:
+        (err, rows) => {
+            console.log(rows);
+            if (rows.length > 0) {
+                res.send(rows[0]);
+            } else {
+                res.send({
+                    error404
+                }); // failed, so return an empty object instead of undefined
+            }
+        }
+    );
+});
+
+/*Get user groups based on specifed optional query parameters*/
+app.get('/gq', (req, res) => {
+    let nToLookup = req.query.param1;
+    let gToLookup = req.query.param2;
+    let mToLookup = req.query.param3;
+
+    console.log(nToLookup);
+    console.log(gToLookup);
+    console.log(mToLookup);
+    //console.log(req.query.param5);    
+    if (nToLookup == '') {
+        nToLookup = null;
+    }
+    if (gToLookup == '') {
+        gToLookup = null;
+    }
+    if (mToLookup == '') {
+        mToLookup = null;
+    }
+    console.log(mToLookup);
+
+    let error404 = "Error 404: Group not found";
+    // db.all() fetches all results from an SQL query into the 'rows' variable:
+    dbGroups.all(
+        'SELECT * FROM groups WHERE (name = $name OR $name IS NULL) AND (gid = $gid OR $gid IS NULL) AND (gid = $gid OR $gid IS NULL) AND (member LIKE $mem OR $member IS NULL )',
+        // parameters to SQL query:
+        {
+
+            $name: nToLookup,
+            $gid: gToLookup,
+            $member: mToLookup,
+            $mem: "%" + mToLookup + "%"
+
+        },
+        // callback function to run when the query finishes:
+        (err, rows) => {
+            console.log(rows);
+            if (rows.length > 0) {
+                res.send(rows[0]);
+            } else {
+                res.send({
+                    error404
+                }); // failed, so return an empty object instead of undefined
+            }
+        }
+    );
+});
 
 
 // start the server at URL: http://localhost:3000/
 app.listen(3000, () => {
-  console.log('Server started at http://localhost:3000/');
+    console.log('Server started at http://localhost:3000/');
 });
